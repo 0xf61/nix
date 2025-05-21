e397244e8da4a11ae6793280cf9666f256a9a642
0xf61 <emirhan@kurt.link>
2025-05-21 16:08:57 +0300

    flake update with nh

--- system/nix/default.nix
+++ system/nix/default.nix
@@ -45,6 +45,8 @@
   programs.nix-ld.enable = true;
   programs.nh = {
     enable = true;
+    clean.enable = true;
+    clean.extraArgs = "--keep-since 4d --keep 3";
     flake = "/home/user/git/nix";
   };
 
